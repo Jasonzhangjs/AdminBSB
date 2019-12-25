410033b10b8644849921693254606da69babcfd9
Jisheng Zhang <jisheng.zhang@avanade.com>
2019-12-25 17:44:45 +0800

    添加项目文件。

---
+++ AdminBSB/Area/Manage/Controllers/NewProController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminBSB.Area.Manage.Controllers
+{
+    public class NewProController : Controller
+    {
+        // GET: NewPro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
