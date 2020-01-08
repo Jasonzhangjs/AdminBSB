a918d28111099d19b1297ae4a520020d64b30d56
jasonzhang <15942640572@sina.cn>
2020-01-08 17:25:41 +0800

    菜单分离

---
+++ AdminBSB/Area/Manage/Controllers/MenuListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminBSB.Area.Manage.Controllers
+{
+    public class MenuListController : Controller
+    {
+        // GET: MenuList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
