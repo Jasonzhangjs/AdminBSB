db78e52c3d256c7dcd68e3798ea51ebc47d907fc
haoyue.yang <haoyue.yang@accenture.com>
2020-01-17 15:27:23 +0800

    Test

--- AdminBSB/Area/Manage/Controllers/MenuListController.cs
+++ AdminBSB/Area/Manage/Controllers/MenuListController.cs
@@ -6,11 +6,25 @@ using System.Web.Mvc;
 
 namespace AdminBSB.Area.Manage.Controllers
 {
+    public class MenuList
+    {
+
+        public int ID { get; set; }
+        public int Name { get; set; }
+    }
+
+
     public class MenuListController : Controller
     {
         // GET: MenuList
         public ActionResult Index()
         {
+
+            List<MenuList> list = new List<MenuList>();
+            list.Add(new MenuList { ID = 1, Name = 22 });
+
+
+
             return View();
         }
     }
