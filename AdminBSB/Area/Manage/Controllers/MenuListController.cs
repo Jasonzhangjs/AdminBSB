f9928cb131bfc89ded18f71e39e2ba4d798a012f
haoyue.yang <haoyue.yang@accenture.com>
2020-01-22 17:38:49 +0800

    menu

--- AdminBSB/Area/Manage/Controllers/MenuListController.cs
+++ AdminBSB/Area/Manage/Controllers/MenuListController.cs
@@ -6,11 +6,20 @@ using System.Web.Mvc;
 
 namespace AdminBSB.Area.Manage.Controllers
 {
-    public class MenuList
+    public class MenuLists
     {
-
+        public List<Menu> Menus { get; set; }
+    }
+    public class Menu
+    {
         public int ID { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
+        public string Controller { get; set; }
+        public int? ParentID { get; set; }
+        public string userID { get; set; }
+        public string Area { get; set; }
+        public int sort { get; set; }
+        public string Role { get; set; }
     }
 
 
@@ -19,13 +28,23 @@ namespace AdminBSB.Area.Manage.Controllers
         // GET: MenuList
         public ActionResult Index()
         {
+            //假的登陆人
+            string UserID = "haoyue.yang";
 
-            List<MenuList> list = new List<MenuList>();
-            list.Add(new MenuList { ID = 1, Name = 22 });
+            //假的list
+            List<Menu> list = new List<Menu>();
+            list.Add(new Menu { ID = 0, Name = "One", Controller = "", ParentID = null, userID = "haoyue.yang", Role = "First Person", Area = "AdminBSB", sort = 0 });
+            list.Add(new Menu { ID = 4, Name = "One-Two", Controller = "OneTwo", ParentID = 0, userID = "haoyue.yang", Role = "First Person", Area = "AdminBSB", sort = 2 });
+            list.Add(new Menu { ID = 1, Name = "One-One", Controller = "OneOne", ParentID = 0, userID = "haoyue.yang", Role = "First Person", Area = "AdminBSB", sort = 1 });
+            list.Add(new Menu { ID = 2, Name = "Two", Controller = "", ParentID = null, userID = "haoyue.yang", Role = "Second Person", Area = "AdminBSB", sort = 0 });
+            list.Add(new Menu { ID = 3, Name = "Two-One", Controller = "TwoOne", ParentID = 2, userID = "haoyue.yang", Role = "Second Person", Area = "AdminBSB", sort = 1 });
 
-
-
-            return View();
+            //返回有权限的list
+            List<Menu> haslist = list.Where(p => p.userID == UserID).OrderBy(i => i.sort).OrderBy(i => i.Role).ToList();
+            //ViewBag.menulist = haslist;
+            return PartialView(new MenuLists { Menus = haslist });
+            //return PartialView(haslist);
+            //return View();
         }
     }
 }
---
+++ AdminBSB/Area/Manage/Controllers/OneOneController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminBSB.Area.Manage.Controllers
+{
+    public class OneOneController : Controller
+    {
+        // GET: One
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
